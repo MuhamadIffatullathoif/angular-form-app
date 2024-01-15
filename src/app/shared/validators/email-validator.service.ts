f018c1cd74528e87d7f2251728186bc6a8c76a50
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-15 11:55:14 +0700

    20. Develop the mail validator

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -1,20 +1,39 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AbstractControl, AsyncValidator, ValidationErrors} from "@angular/forms";
-import {Observable, of} from "rxjs";
+import {delay, Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class EmailValidatorService implements AsyncValidator{
-  constructor() { }
+export class EmailValidatorService implements AsyncValidator {
+  constructor() {
+  }
 
-  validate(control: AbstractControl):  Observable<ValidationErrors | null> {
+  validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email: string = control.value;
 
-    return of({
-      emailTaken: true
-    })
+    const httpCallObservable = new Observable<ValidationErrors | null>(observer => {
+      if (email === 'iffat@gmail.com') {
+        observer.next({emailTaken: true});
+        observer.complete();
+        // return;
+      }
+      observer.next(null);
+      observer.complete();
+    }).pipe(
+      delay(1000)
+    )
+
+    return httpCallObservable;
   }
+
+  // validate(control: AbstractControl):  Observable<ValidationErrors | null> {
+  //   const email: string = control.value;
+  //
+  //   return of({
+  //     emailTaken: true
+  //   })
+  // }
 
 
 }
