a5bc1882c247682583163afbf6accd0827ee7973
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-15 08:24:15 +0700

    09. Help method for errors

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -28,7 +28,28 @@ export class BasicPageComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.myForm.reset(rtx5090)
+    // this.myForm.reset(rtx5090)
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if(!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'This field is required';
+        case 'minlength':
+          return `Minimum ${errors['minlength'].requiredLength} character`;
+      }
+    }
+
+    return null;
   }
 
   onSave(): void {
