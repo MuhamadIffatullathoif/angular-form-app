72d531d0875f64e6f22987adc24e5d7a7923e640
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-15 08:49:01 +0700

    11. Dynamic Validators

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -26,12 +26,38 @@ export class DynamicPageComponent {
   }
 
   onSubmit(): void {
-    if(this.myForm.invalid) {
+    if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
       return;
     }
 
     console.log(this.myForm.value);
     this.myForm.reset();
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'This field is required';
+        case 'minlength':
+          return `Minimum ${errors['minlength'].requiredLength} character`;
+      }
+    }
+    return null;
+  }
+
+  isValidFieldInArray(formArray: FormArray, index: number) {
+    return formArray.controls[index].errors
+      && formArray.controls[index].touched;
   }
 }
